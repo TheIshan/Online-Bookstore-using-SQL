90e7198e9f259c45d89b61e45488babae98ffea2
Ishan Shrivastava <133317507+TheIshan@users.noreply.github.com>
2025-06-27 20:57:22 +0530

    Update Online_BookStore.sql

--- Online_BookStore.sql
+++ Online_BookStore.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE Books (
     Book_ID SERIAL PRIMARY KEY,
     Title VARCHAR(100),
@@ -31,108 +30,96 @@ SELECT * FROM Books;
 SELECT * FROM Customers;
 SELECT * FROM Orders;
 
-
--- Import Data into Books Table
 COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
 FROM 'C:\Program Files\PostgreSQL\Books.csv' 
 CSV HEADER;
 
--- Import Data into Customers Table
 COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
 FROM 'C:\Program Files\PostgreSQL\Customers.csv' 
 CSV HEADER;
 
--- Import Data into Orders Table
 COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
 FROM 'C:\Program Files\PostgreSQL\Orders.csv' 
 CSV HEADER;
 
--- 1) Retrieve all books in the "Fiction" genre:
 select * from Books
 where genre like 'Fiction';
 
-
--- 2) Find books published after the year 1950:
 select * from Books
 where published_year>1958;
 
--- 3) List all customers from the Canada:
 select * from Customers
 where country='Canada';
 
--- 4) Show orders placed in November 2023:
 select * from Orders
 where order_date between '2023-11-01' and '2023-12-01';
 
--- 5) Retrieve the total stock of books available:
 SELECT SUM(stock) AS Total_Stock
 From Books;
 
-
--- 6) Find the details of the most expensive book:
 SELECT * 
 FROM Books
 WHERE price = (SELECT MAX(price) FROM Books);
 
-
--- 7) Show all customers who ordered more than 1 quantity of a book:
 SELECT * FROM Orders 
 WHERE quantity>1;
 
-
--- 8) Retrieve all orders where the total amount exceeds $20:
 SELECT * FROM Orders 
 WHERE total_amount>20;
 
-
--- 9) List all genres available in the Books table:
 SELECT DISTINCT genre FROM Books;
 
-
-
--- 10) Find the book with the lowest stock:
 SELECT * 
 FROM Books
 WHERE stock = (SELECT MIN(stock) FROM Books WHERE stock != 0);
 
--- 11) Calculate the total revenue generated from all orders:
 SELECT SUM(total_amount) AS Total_Amount
 From Orders;
--- Advance Questions : 
 
--- 1) Retrieve the total number of books sold for each genre:
 SELECT b.Genre, SUM(o.Quantity) AS Total_Books_sold
 FROM Orders o
 JOIN Books b ON o.book_id = b.book_id
 GROUP BY b.Genre;
 
--- 2) Find the average price of books in the "Fantasy" genre:
 SELECT Genre, ROUND(AVG(price), 2) AS Average
 FROM Books
 WHERE genre = 'Fantasy'
 GROUP BY Genre;
 
+SELECT o.customer_id, c.name, COUNT(o.Order_id) AS ORDER_COUNT
+FROM orders o
+JOIN customers c ON o.customer_id=c.customer_id
+GROUP BY o.customer_id, c.name
+HAVING COUNT(Order_id) >=2;
 
+SELECT o.Book_id, b.title, COUNT(o.order_id) AS ORDER_COUNT
+FROM orders o
+JOIN books b ON o.book_id=b.book_id
+GROUP BY o.book_id, b.title
+ORDER BY ORDER_COUNT DESC LIMIT 1;
 
+SELECT * FROM books
+WHERE genre ='Fantasy'
+ORDER BY price DESC LIMIT 3;
 
+SELECT b.author, SUM(o.quantity) AS Total_Books_Sold
+FROM orders o
+JOIN books b ON o.book_id=b.book_id
+GROUP BY b.Author;
 
--- 3) List customers who have placed at least 2 orders:
+SELECT DISTINCT c.city, total_amount
+FROM orders o
+JOIN customers c ON o.customer_id=c.customer_id
+WHERE o.total_amount > 30;
 
+SELECT c.customer_id, c.name, SUM(o.total_amount) AS Total_Spent
+FROM orders o
+JOIN customers c ON o.customer_id=c.customer_id
+GROUP BY c.customer_id, c.name
+ORDER BY Total_spent Desc LIMIT 1;
 
--- 4) Find the most frequently ordered book:
-
-
--- 5) Show the top 3 most expensive books of 'Fantasy' Genre :
-
-
-
--- 6) Retrieve the total quantity of books sold by each author:
-
-
--- 7) List the cities where customers who spent over $30 are located:
-
-
--- 8) Find the customer who spent the most on orders:
-
-
---9) Calculate the stock remaining after fulfilling all orders:
+SELECT b.book_id, b.title, b.stock, COALESCE(SUM(o.quantity),0) AS Order_quantity,  
+	b.stock- COALESCE(SUM(o.quantity),0) AS Remaining_Quantity
+FROM books b
+LEFT JOIN orders o ON b.book_id=o.book_id
+GROUP BY b.book_id ORDER BY b.book_id;
